54f59bc40e3fec41b6ed105164f44c5b6a851afa
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-10-14 15:32:28 -0400

    Add all gems and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :tuits
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "tuits#index"
 end
